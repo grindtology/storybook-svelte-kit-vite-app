e7cf115d47e1d338b3a1f726ab949064afdd9fe4
maarten <maartenberkenbosch@gmail.com>
2021-09-27 11:18:42 +0200

    add local data

--- src/lib/common/input/BaseCheckBox.stories.svelte
+++ src/lib/common/input/BaseCheckBox.stories.svelte
@@ -2,6 +2,14 @@
   import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
   import BaseCheckBox from "./BaseCheckBox.svelte"
 
+  let bCheck = false
+const onTap = () =>{ console.log('test')
+bCheck = !bCheck}
+
+// continue
+// make BaseCheckBox > BaseMarker
+// Base marker will only have argTypes
+// CheckBox will have toggle script, and no argTypes (all defined in script tags, see above)
 </script>
 
 
@@ -9,15 +17,15 @@
   title="common/input/BaseCheckBox"
   component={BaseCheckBox}
   argTypes={{
-    bChecked: { control: "boolean" },
+   // bChecked: bCheck,
     sLabel: {controls:"text"},
      sColor: { type: "select", options:["","blue", "yellow", "red", "green"] },
-     onTap: { action: ""} ,
+    // onTap: { action: test()} 
   }}
 />
 
 <Template let:args>
-  <BaseCheckBox {...args}   on:click={args.onTap} />
+  <BaseCheckBox {...args} bChecked={bCheck}  {onTap} />
 </Template>
 
 <Story
