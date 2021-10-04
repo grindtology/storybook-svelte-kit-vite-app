19158302922e2b4543b5d7d8a80bd4f14fd42d57
maarten <maartenberkenbosch@gmail.com>
2021-10-04 10:37:39 +0200

    -

--- src/routes/skill.svelte
+++ src/routes/skill.svelte
@@ -2,7 +2,14 @@
   //-----1.LIB
   import Header from '$lib/gui/Header.svelte'
   import Back from '$lib/icon/Back.svelte'
-
+  //-----2.STORES
+  //-----3.DATA LOCAL
+  const aoSkill = []
+  //-----4.PROPS
+  //-----5.DATA COMPUTED
+  //-----6.DATA WATCH
+  //-----7.INIT
+  //-----8.METHODS
   //-----8.METHODS
 </script>
 
